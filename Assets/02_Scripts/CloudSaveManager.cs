1b585f983b9007cb4f58b04a4309154e142e4bc0
LeeJaeHyun <myleje@gmail.com>
2024-09-25 10:29:35 +0900

    .

--- Assets/02_Scripts/CloudSaveManager.cs
+++ Assets/02_Scripts/CloudSaveManager.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using System.Threading.Tasks;
+using Unity.Services.Core;
 
 public class CloudSaveManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private async void Awake()
     {
+        // UGS 초기화 성공했을 때 호출되는 콜백
+        UnityServices.Initialized += () =>
+        {
+            Debug.Log("UGS 초기화 성공");
+        };
 
-    }
+        UnityServices.InitializeFailed += (ex) =>
+        {
+            Debug.Log($"UGS 초기화 실패 : {ex.Message}");
+        };
 
-    // Update is called once per frame
-    void Update()
-    {
+        // UGS 초기화
+        await UnityServices.InitializeAsync();
 
     }
 }
